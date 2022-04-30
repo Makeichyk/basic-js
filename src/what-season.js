7f53e8d97c08278e65fb60b690f53ce69d0ef9ef
Makeichyk <jaroslau1@gmail.com>
2022-04-30 17:06:54 +0400

    fix: 100% solve what-the-season task

--- src/what-season.js
+++ src/what-season.js
@@ -2,33 +2,78 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
- * @param {Date | FakeDate} date real or fake date
+ *
+ * @param {Date | FakeDate} dateObj real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
+// function getSeason(dateObj) {
+// 	const winter = [11, 0, 1];
+// 	const spring = [2, 3, 4];
+// 	const summer = [5, 6, 7];
+// 	const fall = [8, 9, 10];
+// 	
+// 	
+
+// 	if (dateObj === undefined) {
+// 		return stringError;
+// 	}
+
+// 	if (month >= 0 && month <= 11) {
+// 		if (winter.includes(month)) {
+// 			return 'winter';
+// 		}
+// 		if (spring.includes(month)) {
+// 			return 'spring';
+// 		}
+// 		if (summer.includes(month)) {
+// 			return 'summer';
+// 		}
+// 		if (fall.includes(month)) {
+// 			return 'autumn';
+// 		}
+// 	}
+// }
+
 function getSeason(dateObj) {
-let stringError = 'Unable to determine the time of year!' ;
-  if (dateObj.toString() !== 'Invalid Date'){
-    let month = dateObj.getMonth();
-    let seasons = ['winter','spring','summer','autumn'];
-    if (month === 11 || month <= 1){
-      return seasons[0];
-    }else if(month <= 4){
-      return seasons[1];
-    }else if(month <=7){
-      return seasons[2];
-    }else if(month <=10){
-      return seasons[3]
+  const WINTER = [11, 0, 1];
+  const SPRING = [2, 3, 4];
+  const SUMMER = [5, 6, 7];
+  const FALL = [8, 9, 10];
+  const STRING_ERROR = 'Unable to determine the time of year!';
+  const ERROR_MESSAGE = 'Invalid date!'
+  
+
+  if (dateObj === undefined) {
+    return STRING_ERROR;
+  }
+
+  try {
+    dateObj.getUTCDate();
+  } catch (error) {
+    throw Error(ERROR_MESSAGE);
+  }
+
+  const month = dateObj.getMonth();
+  if (month >= 0 && month <= 11) {
+    if (WINTER.includes(month)) {
+      return 'winter';
     }
-  }else{
-    return new Error ('Invalid date!')
+    if (SPRING.includes(month)) {
+      return 'spring';
+    }
+    if (SUMMER.includes(month)) {
+      return 'summer';
+    }
+    if (FALL.includes(month)) {
+      return 'autumn';
+    }
   }
 }
 module.exports = {
-  getSeason
+	getSeason,
 };
